2b1ca74a6829e47cd1d701d50f4e7f72fde8b14a
ly931126 <1056548637@qq.com>
2017-02-14 11:27:43 +0800

    添加StringUtils方法：判断字符串是否为空

--- basetoolslibrary/src/main/java/com/excellence/basetoolslibrary/utils/StringUtils.java
+++ basetoolslibrary/src/main/java/com/excellence/basetoolslibrary/utils/StringUtils.java
@@ -4,11 +4,32 @@ package com.excellence.basetoolslibrary.utils;
  * Created by ZhangWei on 2017/2/13.
  */
 
+import android.text.TextUtils;
+
 /**
  * 字符串相关
  */
 public class StringUtils
 {
-  // 李叶Test
-    public static final String TAG = StringUtils.class.getSimpleName();
+    /**
+     * 判断字符串是否为空
+     *
+     * @param text 只读序列 {@link String }
+     * @return
+     */
+	public static boolean isEmpty(String text)
+    {
+        return TextUtils.isEmpty(text);
+    }
+
+    /**
+     * 判断字符串是否为空
+     *
+     * @param text 可读可写序列 {@link CharSequence}
+     * @return
+     */
+    public static boolean isEmpty(CharSequence text)
+    {
+        return isEmpty(text.toString());
+    }
 }
