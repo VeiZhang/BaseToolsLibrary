811a4a80452402be9fcbdd4e3557ca7388c0525c
Tiimor <tiimor@qq.com>
2018-09-11 12:06:26 +0800

    优化代码

--- basetoolslibrary/src/main/java/com/excellence/basetoolslibrary/utils/AppUtils.java
+++ basetoolslibrary/src/main/java/com/excellence/basetoolslibrary/utils/AppUtils.java
@@ -291,22 +291,22 @@ public class AppUtils
 			typeArgs[1] = String.class;
 			typeArgs[2] = DisplayMetrics.class;
 			typeArgs[3] = Integer.TYPE;
-			Method pkgParser_parsePackageMtd = pkgParserCls.getDeclaredMethod("parsePackage", typeArgs);
+			Method parsePackageMtd = pkgParserCls.getDeclaredMethod("parsePackage", typeArgs);
 			valueArgs = new Object[4];
 			valueArgs[0] = new File(apkPath);
 			valueArgs[1] = apkPath;
 			valueArgs[2] = metrics;
 			valueArgs[3] = PackageManager.GET_SIGNATURES;
-			Object pkgParserPkg = pkgParser_parsePackageMtd.invoke(pkgParser, valueArgs);
+			Object pkgParserPkg = parsePackageMtd.invoke(pkgParser, valueArgs);
 
 			typeArgs = new Class[2];
 			typeArgs[0] = pkgParserPkg.getClass();
 			typeArgs[1] = Integer.TYPE;
-			Method pkgParser_collectCertificatesMtd = pkgParserCls.getDeclaredMethod("collectCertificates", typeArgs);
+			Method collectCertificatesMtd = pkgParserCls.getDeclaredMethod("collectCertificates", typeArgs);
 			valueArgs = new Object[2];
 			valueArgs[0] = pkgParserPkg;
 			valueArgs[1] = PackageManager.GET_SIGNATURES;
-			pkgParser_collectCertificatesMtd.invoke(pkgParser, valueArgs);
+			collectCertificatesMtd.invoke(pkgParser, valueArgs);
 
 			Field packageInfoFld = pkgParserPkg.getClass().getDeclaredField("mSignatures");
 			Signature[] signatures = (Signature[]) packageInfoFld.get(pkgParserPkg);
