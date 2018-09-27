961e021727a703084410453ae051f1468c2881a3
Tiimor <tiimor@qq.com>
2018-09-27 14:11:18 +0800

    解决第三方库FileProvider冲突

---
+++ basetoolslibrary/src/main/java/com/excellence/basetoolslibrary/assist/Provider.java
@@ -0,0 +1,15 @@
+package com.excellence.basetoolslibrary.assist;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * <pre>
+ *     author : VeiZhang
+ *     blog   : http://tiimor.cn
+ *     time   : 2018/9/27
+ *     desc   : 自定义{@link FileProvider}，避免与其他第三方库依赖冲突导致：编译失败、异常
+ * </pre> 
+ */
+public class Provider extends FileProvider
+{
+}
