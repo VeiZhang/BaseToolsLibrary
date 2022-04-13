a77a7238c77a03fa6c10225d7761cb9de1409745
Tiimor <tiimor@qq.com>
2022-04-13 11:45:13 +0800

    kt替换CommonAdapter

---
+++ basetoolslibrary/src/main/java/com/excellence/basetoolslibrary/baseadapter/CommonAdapter.kt
@@ -0,0 +1,33 @@
+package com.excellence.basetoolslibrary.baseadapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+
+/**
+ * <pre>
+ *     author : VeiZhang
+ *     blog   : http://tiimor.cn
+ *     time   : 2022/4/13
+ *     desc   : ListView、GridView通用适配器
+ * </pre>
+ */
+abstract class CommonAdapter<T> : MultiItemTypeAdapter<T> {
+
+    private var mLayoutId: Int
+
+    constructor(data: Array<T>?, @LayoutRes layoutId: Int) : this(if (data == null) null else listOf(*data), layoutId)
+
+    constructor(data: List<T>?, @LayoutRes layoutId: Int) : super(data) {
+        mLayoutId = layoutId
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val viewHolder = ViewHolder.getViewHolder(parent.context, convertView, parent, mLayoutId)
+        convert(viewHolder, getItem(position), position)
+        return viewHolder.convertView
+    }
+
+    abstract fun convert(viewHolder: ViewHolder?, item: T?, position: Int)
+
+}
